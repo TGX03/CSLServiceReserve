d2d7c5b737d20648c343b6653c6f50849a579c86
tgx003 <tgx003@gmail.com>
2022-09-13 23:40:06 +0200

    Permanent initialization

--- CSLServiceReserve/CSLServiceReserve/KHVehicleManager.cs
+++ CSLServiceReserve/CSLServiceReserve/KHVehicleManager.cs
@@ -42,6 +42,7 @@ namespace CSLServiceReserve
                                 NormalVehicleCount++;
                             }
                         }
+                        Initialized = true;
                     }
                 }
             }
